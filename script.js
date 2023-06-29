9cafd0421c933b95bf20d77589297afe45027c85
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-06-29 21:18:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    var x=document.getElementById("form1");
+	for(var i=0;i<x.length;i++)
+		{
+			if(x.elements[i].value!="submit")
+			{
+				console.log(x.elements[i].value);
+			}
+		}
 
 }
